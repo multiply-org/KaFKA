a4595ed78bbffda25ae0d1539209c5b7b906bbed
npounder <nicola.pounder@assimila.eu>
2019-06-12 13:50:44 +0100

    add prior uncertainty and default prior

--- kafka/inference/temporal_prior.py
+++ kafka/inference/temporal_prior.py
@@ -7,6 +7,10 @@ import datetime as dt
 from .utils import block_diag
 from .narrowbandSAIL_tools import sail_prior_values
 
+# Set up logging
+import logging
+LOG = logging.getLogger(__name__+".temporal_prior")
+
 '''
 def prior_values(time):
     mean, covar, inv_covar = sail_prior_values()
@@ -20,19 +24,16 @@ def prior_values(time):
     return mean, covar, inv_covar
 '''
 
+
 class TemporalSAILPrior(object):
-    def __init__(self, parameter_list, state_mask, LAI = None):
+    def __init__(self, parameter_list, state_mask, LAI=None, LAI_unc=None):
         self.parameter_list = parameter_list
         if isinstance(state_mask, (np.ndarray, np.generic)):
             self.state_mask = state_mask
         else:
             self.state_mask = self._read_mask(state_mask)
-        #self.time_grid = time_grid
         self.LAI = LAI
-            #mean, c_prior, c_inv_prior = sail_prior_values()
-            #self.mean = mean
-            #self.covar = c_prior
-            #self.inv_covar = c_inv_prior
+        self.LAI_unc = LAI_unc
 
 
     def _read_mask(self, fname):
@@ -46,22 +47,28 @@ class TemporalSAILPrior(object):
         return mask
 
     def process_prior(self, time, inv_cov=True):
-        # Presumably, self._inference_prior has some method to retrieve
-        # a bunch of files for a given date...
 
-        #mean, covar, inv_covar = prior_values(time)
+        # mean, covar, inv_covar = prior_values(time)
         mean, covar, inv_covar = sail_prior_values()
         lai_position = 6
-        mean[lai_position] = self.LAI[time.date()]
+        try:
+            mean[lai_position] = self.LAI[time.date()]
+        except KeyError: #use default prior if none in LAI
+            LOG.info("No prior LAI for {} using default = {}".format(
+                    time.date(), mean[lai_position]))
+        if self.LAI_unc is not None:
+            try:
+                inv_covar[lai_position, lai_position] = self.LAI_unc[time.date()]
+            except KeyError:  # use default prior if none in LAI
+                LOG.info("No prior LAI_unc for {} using default = {}".format(
+                          time.date(), inv_covar[lai_position]))
         n_pixels = self.state_mask.sum()
-        x0 = np.array([mean for i in range(n_pixels)]).flatten()
+        x0 = np.array([mean for _ in range(n_pixels)]).flatten()
         if inv_cov:
-            inv_covar_list = [inv_covar for m in range(n_pixels)]
+            inv_covar_list = [inv_covar for _ in range(n_pixels)]
             inv_covar = block_diag(inv_covar_list, dtype=np.float32)
             return x0, inv_covar
         else:
-            covar_list = [covar for m in range(n_pixels)]
+            covar_list = [covar for _ in range(n_pixels)]
             covar = block_diag(covar_list, dtype=np.float32)
             return x0, covar
-
-
